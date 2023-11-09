772cb5b51561517fab8ae1a2ddf6aee77517b718
alirezangh <alireza20002485@gmail.com>
2023-11-09 14:20:27 -0800

    finish-course-slider-with-swiper

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,3 +1,42 @@
 let $ = document;
 import { NavBar } from "../component/nav-bar-menu/_nav-menu.js";
-window.customElements.define("navbar-tg",NavBar)
+import { productSlider } from "./course-slider.js";
+window.customElements.define("navbar-tg", NavBar);
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop: true,
+  breakpoints: {
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 5,
+    },
+
+    540: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 7,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 8,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
