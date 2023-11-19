06d3aabeba06d3160ab46ce7f922fa8dcc4f1fd9
alirezangh <alireza20002485@gmail.com>
2023-11-19 04:16:19 -0800

    set navbar and breadcrumb

---
+++ assest/js/all-article.js
@@ -0,0 +1,6 @@
+let $ = document;
+import { NavBar } from "../component/nav-bar-menu/_nav-menu.js";
+import { Footer } from "../component/footer/footer.js";
+import { TopBtn } from "../component/go-top-btn/go-top-btn.js";
+import { Aside } from "../component/aside-section-col-3/aside.js";
+window.customElements.define("navbar-tg", NavBar);
