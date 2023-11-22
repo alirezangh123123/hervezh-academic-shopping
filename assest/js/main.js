a3af3df2386d04a4e1d6cc4955688c33499d9a98
alirezangh <alireza20002485@gmail.com>
2023-11-22 06:26:34 -0800

    finish search-header-section

--- assest/js/main.js
+++ assest/js/main.js
@@ -4,9 +4,11 @@ import { articeSlider } from "../component/articles/_article-comp.js";
 import { productSlider } from "./course-slider.js";
 import { Footer } from "../component/footer/footer.js";
 import { TopBtn } from "../component/go-top-btn/go-top-btn.js";
+import { product } from "../db/product.js";
 window.customElements.define("navbar-tg", NavBar);
 window.customElements.define("footer-tg", Footer);
 window.customElements.define("top-tg", TopBtn);
+
 // course-slider
 var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
@@ -69,5 +71,57 @@ var blogBoxSwiper = new Swiper(".article-swiper", {
     },
   },
 });
-// relate-products-slider
-
+//search-section form
+const getForm = $.getElementsByClassName("search-field-form");
+const getSearchINp = $.getElementById("search-field-inp");
+const getSubFormBtn = $.getElementById("sub-form-btn");
+const getLIst = $.getElementById("suggest-list");
+let showSuggestions = () => {
+  let getInpValue = getSearchINp.value.trim();
+  let getResult = product.filter((product) => {
+    if (product.title.includes(getInpValue)) {
+      return product.title;
+    }
+  });
+  if (getResult) {
+    getLIst.innerHTML = "";
+    // make list stracture
+    getResult.map((target) => {
+      let liElem, linkElem;
+      liElem = $.createElement("li");
+      liElem.className =
+        "suggest-item py-2 mb-1 border-bottom d-flex justify-content-around align-items-center px-3";
+      linkElem = $.createElement("a");
+      linkElem.setAttribute("href", `product.html?id=${target.id}`);
+      linkElem.textContent = target.title;
+      linkElem.className = "target-link";
+      liElem.append(linkElem);
+      getLIst.append(liElem);
+    });
+  }
+  if (getSearchINp.value !== "") {
+    getLIst.classList.add("active");
+  } else {
+    getLIst.classList.remove("active");
+  }
+  let listItems = $.querySelectorAll("li.suggest-item");
+  listItems.forEach((item) => {
+    let textContent = item.lastElementChild.textContent;
+    item.addEventListener("click", (event) => {
+      event.preventDefault();
+      getSearchINp.value = textContent;
+      getLIst.classList.remove("active");
+    });
+  });
+};
+// go to correct location
+getSubFormBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let findAddress = $.querySelector("a.target-link");
+  let correctLink = findAddress.href;
+  location.href = correctLink;
+});
+getSearchINp.addEventListener("keyup", showSuggestions);
+getForm[0].addEventListener("submit", (event) => {
+  event.preventDefault();
+});
