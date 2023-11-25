1f2b37b62f7e4c9bce4c746435bbf1ae7fd90ac3
alirezangh <alireza20002485@gmail.com>
2023-11-25 01:43:25 -0800

    finish-set-catagory-shop-page

--- assest/js/main-shop.js
+++ assest/js/main-shop.js
@@ -32,7 +32,11 @@ let setProductCard = product.forEach((item) => {
       <div
         class="card-footer bg-white courses-card-footer rounded-bottom-3 d-flex justify-content-between"
       >
-        <span class="price">${item.price}</span>
+      <div class="price-wrapper d-flex">
+      <span class="price text-end mx-1 order-2">${item.price}</span>
+      <span class="price text-end  ">تومان</span>
+      </div>
+       
         <span class="member">
           <i class="fa fa-users"></i>
           ${item.member}
@@ -54,11 +58,13 @@ const suggestionList = $.getElementById("suggest-list");
 let startSearch = () => {
   searchInp.classList.add("active");
   let searchInpValue = searchInp.value.toUpperCase().trim();
-  let searchData = allDatas.filter((data) => {
-    if (data.title.includes(searchInpValue)) {
-      return data.title;
-    }
-  }).slice(0,5);;
+  let searchData = allDatas
+    .filter((data) => {
+      if (data.title.includes(searchInpValue)) {
+        return data.title;
+      }
+    })
+    .slice(0, 5);
 
   if (searchData) {
     suggestionList.innerHTML = "";
@@ -90,29 +96,188 @@ let startSearch = () => {
     suggestionList.classList.remove("active");
     searchInp.classList.remove("active");
   }
-  let chooseItem = $.querySelectorAll("li.suggest-item")
-  chooseItem.forEach((target)=>{
-    let targetTitle=target.lastElementChild.textContent;
-    target.addEventListener("click",(event)=>{
-event.preventDefault();
-searchInp.value = targetTitle;
-suggestionList.classList.remove("active");
-suggestionList.innerHTML = target.innerHTML;
-    })
-  })
+  let chooseItem = $.querySelectorAll("li.suggest-item");
+  chooseItem.forEach((target) => {
+    let targetTitle = target.lastElementChild.textContent;
+    target.addEventListener("click", (event) => {
+      event.preventDefault();
+      searchInp.value = targetTitle;
+      suggestionList.classList.remove("active");
+      suggestionList.innerHTML = target.innerHTML;
+    });
+  });
   // go to correct address
-  subLinkBtn[0].addEventListener("click",(event)=>{
-    
+  subLinkBtn[0].addEventListener("click", (event) => {
     let getLinkAddress = $.querySelector("a.target-link");
     let getHref = getLinkAddress.href;
     console.log(getHref);
     console.log(getLinkAddress);
-    
-    location.href = getHref
-  })
+
+    location.href = getHref;
+  });
 };
 searchInp.addEventListener("keyup", startSearch);
-// increase input-size
-// searchInp.addEventListener("click", (event) => {
-//   event.target.classList.toggle("active");
-// });
+//
+//
+// sort products based on their prices descending
+//
+//
+let expensiveBtn = $.getElementById("expensive");
+let cheapestBtn = $.getElementById("cheapest");
+let popularBtn = $.getElementById("most-popular");
+let defaultBtn = $.getElementById("default-mode");
+let catgoryTitle = $.getElementById("catagory-title");
+let descendingMode = () => {
+  let sortByPriceDescending = (a, b) => {
+    const price_A = parseInt(a.price.replace(/\D/g, ``), 10);
+    const price_B = parseInt(b.price.replace(/\D/g, ``), 10);
+    return price_B - price_A;
+  };
+
+  const sortedProducts = product.sort(sortByPriceDescending);
+
+  allProductsWrapper.innerHTML = "";
+  sortedProducts.forEach((item) => {
+    allProductsWrapper.insertAdjacentHTML(
+      `beforeend`,
+      `
+      <div class="col-md-4 col-lg-3 mb-sm-6 mb-md-7">
+        <div class="card courses-card mt-4 px-3 py-1">
+          <div class="card-img courses-card-img">
+            <img src="${item.cover}" class="img-fluid rounded-5" alt="" />
+          </div>
+          <div class="card-body courses-card-body">
+            <div class="custom-card-title text-center">
+              <a href="product.html?id=${item.id}" class="fw-bold text-dark">${item.title}</a>
+            </div>
+          </div>
+          <div class="card-footer bg-white courses-card-footer rounded-bottom-3 d-flex justify-content-between">
+            <div class="price-wrapper d-flex">
+              <span class="price text-end mx-1 order-2">${item.price}</span>
+              <span class="price text-end  ">تومان</span>
+            </div>
+            <span class="member">
+              <i class="fa fa-users"></i>
+              ${item.member}
+            </span>
+          </div>
+        </div>
+      </div>
+    `
+    );
+  });
+  catgoryTitle.innerHTML = "گرانترین";
+};
+let adscendingMode = () => {
+  let sortByPriceDescending = (a, b) => {
+    const price_A = parseInt(a.price.replace(/\D/g, ``), 10);
+    const price_B = parseInt(b.price.replace(/\D/g, ``), 10);
+    return price_A - price_B;
+  };
+  const sortedProducts = product.sort(sortByPriceDescending);
+  allProductsWrapper.innerHTML = "";
+  sortedProducts.forEach((item) => {
+    allProductsWrapper.insertAdjacentHTML(
+      `beforeend`,
+      `
+      <div class="col-md-4 col-lg-3 mb-sm-6 mb-md-7">
+        <div class="card courses-card mt-4 px-3 py-1">
+          <div class="card-img courses-card-img">
+            <img src="${item.cover}" class="img-fluid rounded-5" alt="" />
+          </div>
+          <div class="card-body courses-card-body">
+            <div class="custom-card-title text-center">
+              <a href="product.html?id=${item.id}" class="fw-bold text-dark">${item.title}</a>
+            </div>
+          </div>
+          <div class="card-footer bg-white courses-card-footer rounded-bottom-3 d-flex justify-content-between">
+            <div class="price-wrapper d-flex">
+              <span class="price text-end mx-1 order-2">${item.price}</span>
+              <span class="price text-end  ">تومان</span>
+            </div>
+            <span class="member">
+              <i class="fa fa-users"></i>
+              ${item.member}
+            </span>
+          </div>
+        </div>
+      </div>
+    `
+    );
+  });
+  catgoryTitle.innerHTML = "ارزانترین";
+};
+let mostPopularMode = () => {
+  const sortedProducts = product.sort((member_A, member_B) => {
+    return member_B.member - member_A.member;
+  });
+  allProductsWrapper.innerHTML = "";
+  sortedProducts.forEach((item) => {
+    allProductsWrapper.insertAdjacentHTML(
+      `beforeend`,
+      `
+      <div class="col-md-4 col-lg-3 mb-sm-6 mb-md-7">
+        <div class="card courses-card mt-4 px-3 py-1">
+          <div class="card-img courses-card-img">
+            <img src="${item.cover}" class="img-fluid rounded-5" alt="" />
+          </div>
+          <div class="card-body courses-card-body">
+            <div class="custom-card-title text-center">
+              <a href="product.html?id=${item.id}" class="fw-bold text-dark">${item.title}</a>
+            </div>
+          </div>
+          <div class="card-footer bg-white courses-card-footer rounded-bottom-3 d-flex justify-content-between">
+            <div class="price-wrapper d-flex">
+              <span class="price text-end mx-1 order-2">${item.price}</span>
+              <span class="price text-end  ">تومان</span>
+            </div>
+            <span class="member">
+              <i class="fa fa-users"></i>
+              ${item.member}
+            </span>
+          </div>
+        </div>
+      </div>
+    `
+    );
+  });
+  catgoryTitle.innerHTML = "محبوب ترین";
+};
+
+window.addEventListener("load", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const sortParam = urlParams.get("sort");
+
+  if (sortParam === "expensive") {
+    descendingMode();
+  }
+  if (sortParam === "cheaper") {
+    adscendingMode();
+  }
+  if (sortParam === "popular") {
+    mostPopularMode();
+  }
+
+  const preLoaderWrapper = $.getElementsByClassName("preload-container");
+  preLoaderWrapper[0].classList.add("hidden");
+});
+
+expensiveBtn.addEventListener("click", () => {
+  const url = new URL(window.location.href);
+  url.searchParams.set("sort", "expensive");
+  window.location.href = url;
+});
+cheapestBtn.addEventListener("click", () => {
+  const url = new URL(window.location.href);
+  url.searchParams.set("sort", "cheaper");
+  window.location.href = url;
+});
+popularBtn.addEventListener("click", () => {
+  const url = new URL(window.location.href);
+  url.searchParams.set("sort", "popular");
+  window.location.href = url;
+});
+defaultBtn.addEventListener("click", () => {
+  location.href = "shop.html";
+  catgoryTitle.innerHTML = "پیش فرض";
+});
