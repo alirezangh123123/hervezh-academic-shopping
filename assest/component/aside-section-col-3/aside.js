5a997400b6470f02eca0ef2af590d055324d4bf0
alirezangh <alireza20002485@gmail.com>
2023-12-02 13:50:52 -0800

    fix-search-field-of-aside-section

--- assest/component/aside-section-col-3/aside.js
+++ assest/component/aside-section-col-3/aside.js
@@ -1,24 +1,41 @@
 let $ = document;
+import { product } from "../../db/product.js";
+import { articles } from "../../db/article.js";
+let setAllData = product.concat(articles);
+
 const asideTemplate = document.createElement("template");
 asideTemplate.innerHTML = `
 <link rel="stylesheet" href="/assest/css/font-awesome.css" />
 <link rel="stylesheet" href="/assest/css/bootstrap.css" />
 <link rel="stylesheet" href="/assest/css/style.css" />
 <link rel="stylesheet" href="/assest/component/aside-section-col-3/aside.css" />
+<link rel="stylesheet" href="/assest/css/shop.css" />
 
 <aside class="  side-bar-wrapper">
 <div class="side-bar-search-inner col-12">
   <div class="search-section bg-white mt-2">
-    <form class="p-3 position-relative search-form-section">
-      <input
-        type="search"
-        class="form-control text-end rounded-0"
-        placeholder="...جستجو"
-      />
-      <i
-        class="fa fa-magnifying-glass text-secondary position-absolute"
-      ></i>
-    </form>
+  <form class="w-100 search-section search-field-form">
+                    <div class="w-100 input-wrapper position-relative">
+                      <a
+                        href="#"
+                        class="target-link-location position-absolute z-3"
+                      >
+                        <i class="fa fa-magnifying-glass"></i>
+                      </a>
+                      <input
+                        type="search"
+                        id="search-field"
+                        class="form-control search-header-input form-control-lg py-3 search-prod text-end form-control rounded-0 form-control-lg pe-0 px-6"
+                        placeholder="...جستجو کنید"
+                        style="font-size: 15px"
+                        autocomplete="off"
+                      />
+                    </div>
+                    <ul
+                      class="z-4 w-100 py-2 me-3 text-end pe-2 suggest-list list-unstyled d-flex flex-column align-items-start text-dark"
+                      id="suggest-list"
+                    ></ul>
+                  </form>
   </div>
 </div>
 <div
@@ -81,6 +98,71 @@ class Aside extends HTMLElement {
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.append(asideTemplate.content.cloneNode(true));
+    this.SearchInput = this.shadowRoot.querySelector("#search-field");
+    this.getSearchForm = this.shadowRoot.querySelector(".search-field-form");
+    this.SubLinkBtn = this.shadowRoot.querySelector(".target-link-location");
+    this.suggestionList = this.shadowRoot.querySelector("#suggest-list");
+    this.SearchInput.addEventListener("keyup", (event) => {
+      event.target.classList.add("active");
+      let searchInpValue = event.target.value.toUpperCase().trim();
+      let searchData = setAllData
+        .filter((data) => {
+          if (data.title.includes(searchInpValue)) {
+            return data.title;
+          }
+        })
+        .slice(0, 5);
+      if (searchData) {
+        this.suggestionList.innerHTML = "";
+        searchData.map((target) => {
+          let liElem, linkElem, labelElem;
+          liElem = $.createElement("li");
+          liElem.className =
+            "suggest-item ms-auto w-100   py-1  border-bottom d-flex justify-content-between align-items-center px-1";
+          linkElem = $.createElement("a");
+          if (target.label == "#مقاله") {
+            linkElem.setAttribute(
+              "href",
+              `article-content.html?id=${target.id}`
+            );
+          } else {
+            linkElem.setAttribute("href", `product.html?id=${target.id}`);
+          }
+          linkElem.textContent = target.title;
+          linkElem.className = "target-link text-end";
+          labelElem = $.createElement("label");
+          labelElem.className = "text-dark text-start";
+          labelElem.textContent = target.label;
+          labelElem.style.fontSize = "14px";
+          labelElem.style.width = "60%";
+          liElem.append(labelElem, linkElem);
+          this.suggestionList.append(liElem);
+        });
+      }
+      if (this.SearchInput.value.trim() !== "") {
+        this.suggestionList.classList.add("active");
+      } else {
+        this.suggestionList.classList.remove("active");
+        this.SearchInput.classList.remove("active");
+        this.suggestionList.innerHTML = "";
+      }
+      let chooseItem = this.shadowRoot.querySelectorAll("li.suggest-item");
+
+      chooseItem.forEach((target) => {
+        let targetTitle = target.lastElementChild.textContent;
+        target.addEventListener("click", (event) => {
+          event.preventDefault();
+          this.SearchInput.value = targetTitle;
+          this.suggestionList.classList.remove("active");
+          this.suggestionList.innerHTML = target.innerHTML;
+        });
+      });
+      this.SubLinkBtn.addEventListener("click", () => {
+        let getLinkAddress = this.shadowRoot.querySelector("a.target-link");
+        let getHref = getLinkAddress.href;
+        location.href = getHref;
+      });
+    });
   }
 }
-export {Aside}
+export { Aside };
