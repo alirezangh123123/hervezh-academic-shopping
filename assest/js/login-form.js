1bc1566fb0eb24c5da253c74b32bd75b2c557adc
alirezangh <alireza20002485@gmail.com>
2023-12-06 13:26:43 -0800

    set-login-form-first-version

--- assest/js/login-form.js
+++ assest/js/login-form.js
@@ -7,7 +7,7 @@ const getSignUpUserName = $.getElementById("sign-up-user-name");
 const getSignUpPassword = $.getElementById("sign-up-password");
 const getSignUpEmail = $.getElementById("sign-up-email");
 const getSignUpForm = $.getElementsByClassName("sign-up-form")[0];
-
+const getLoginForm = $.getElementsByClassName("login-form")[0];
 getToggleBtn[0].addEventListener("click", (event) => {
   getMainContainer[0].classList.add("active");
 });
@@ -52,5 +52,38 @@ getSignUpForm.addEventListener("submit", async (event) => {
     location.href = "../../index.html";
   } catch (err) {
     console.log("something went wrong", err);
+  }
+});
+let userLogin = async (event) => {};
+window.addEventListener("load", async (event) => {
+  try {
+    let getUserData = await fetch(
+      `https://userbasketproject-default-rtdb.firebaseio.com/userData.json`
+    );
+    let getResponse = await getUserData.json();
+    let changeToArray = Object.entries(getResponse);
+    getLoginForm.addEventListener("submit", (subEvent) => {
+      subEvent.preventDefault();
+      let getUserNameValue =
+        getLoginForm.querySelector("#login-user-name").value;
+      let getUserPasswordValue =
+        getLoginForm.querySelector("#login-password").value;
+      let showDatas = changeToArray.find((userData) => {
+        if (
+          getUserNameValue === userData[1].name &&
+          getUserPasswordValue == userData[1].password
+        ) {
+          return userData[1].name && userData[1].password;
+        }
+      });
+      if (showDatas) {
+        setCookie("user-data", showDatas[1].name, 4);
+        location.href = "/index.html";
+      } else if (!showDatas) {
+        console.log("چنین کاربری در وبسایت وجود ندارد");
+      }
+    });
+  } catch (e) {
+    console.log("something went wrong", e);
   }
 });
