793f22758bbd8cfe3d6f255d922445d709352834
alirezangh <alireza20002485@gmail.com>
2023-12-10 00:53:58 -0800

    finish-sign-up-validation

--- assest/js/login-form.js
+++ assest/js/login-form.js
@@ -5,16 +5,77 @@ const getCloseBtn = $.getElementsByClassName("close-btn");
 const getRegisterBtn = $.getElementById("reg-btn");
 const getSignUpUserName = $.getElementById("sign-up-user-name");
 const getSignUpPassword = $.getElementById("sign-up-password");
-const getSignUpEmail = $.getElementById("sign-up-email");
+const getSignUpPasswordRep = $.getElementById("r-sing-up-password");
 const getSignUpForm = $.getElementsByClassName("sign-up-form")[0];
 const getLoginForm = $.getElementsByClassName("login-form")[0];
+const toastWrapper = $.getElementsByClassName("toast-container");
+let toastAlert = (massage) => {
+  toastWrapper[0].innerHTML = "";
+  toastWrapper[0].insertAdjacentHTML(
+    `beforeend`,
+    `
+<div id="liveToast" class="toast bg-primary" role="alert" aria-live="assertive" aria-atomic="true">
+<div class="toast-header bg-primary">
+<small class="fw-bold">هم اکنون</small>
+  <strong class="ms-auto  fw-bold">خطا</strong>
+  <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+</div>
+<div class="toast-body text-end  fw-bold">
+  ${massage}
+</div>
+</div>
+`
+  );
+  const toastLiveExample = document.getElementById("liveToast");
+
+  const toastBootstrap = new bootstrap.Toast(toastLiveExample);
+
+  toastBootstrap.show();
+};
+//regex patterns for register and login
+const userNamePattern = /^[a-zA-Z0-9\u0600-\u06FF._-]{8,12}/;
+const signUpPasswordPattern =
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,12}$/;
+//check validation
+getSignUpUserName.addEventListener("input", () => {
+  validationInputs();
+});
+getSignUpPassword.addEventListener("input", () => {
+  validationInputs();
+});
+getSignUpPasswordRep.addEventListener("input", () => {
+  validationInputs();
+});
+let validationInputs = () => {
+  if (!userNamePattern.test(getSignUpUserName.value)) {
+    toastAlert(
+      "نام کاربری باید شامل حروف کوچک و بزرگ و اعداد وحداقل ۸کاراکتروحداکثر۱۲کاراکتر باشد"
+    );
+
+    return false;
+  }
+  if (!signUpPasswordPattern.test(getSignUpPassword.value)) {
+    toastAlert(
+      " وحداقل ۶وحداکثر۱۲کاراکتر باشد\n.واعدادباشد special-characterکلمه عبور باید شامل حداقل یک حرف بزرگ و یک  "
+    );
+
+    return false;
+  }
+  if (getSignUpPasswordRep.value !== getSignUpPassword.value) {
+    toastAlert("فیلد های رمز عبور یکسان نیستند");
+    return false;
+  }
+  return true;
+};
+
+//toggle btn to  show sign up form
 getToggleBtn[0].addEventListener("click", (event) => {
   getMainContainer[0].classList.add("active");
 });
 getCloseBtn[0].addEventListener("click", () => {
   getMainContainer[0].classList.remove("active");
 });
-// set cookie and set api
+//post data if all inputs obbey from patterns
 let sendUserData = async (userData) => {
   try {
     let fetchData = await fetch(
@@ -33,6 +94,7 @@ let sendUserData = async (userData) => {
     console.log("something went wrong", err);
   }
 };
+// set cookie and set api
 let setCookie = (cookieName, cookieValue, expDay) => {
   let getDate = new Date();
   getDate.setTime(getDate.getTime() + expDay * 24 * 60 * 60 * 1000);
@@ -41,15 +103,17 @@ let setCookie = (cookieName, cookieValue, expDay) => {
 getSignUpForm.addEventListener("submit", async (event) => {
   try {
     event.preventDefault();
-    let userData = {
-      name: getSignUpForm.querySelector("#sign-up-user-name").value,
-      password: getSignUpForm.querySelector("#sign-up-password").value,
-      email: getSignUpForm.querySelector("#sign-up-email").value,
-    };
-    setCookie("user-data", `${userData.name}`, 4);
-    await sendUserData(userData);
 
-    location.href = "../../index.html";
+    if (validationInputs()) {
+      let userData = {
+        name: getSignUpForm.querySelector("#sign-up-user-name").value,
+        password: getSignUpForm.querySelector("#sign-up-password").value,
+      };
+      setCookie("user-data", `${userData.name}`, 4);
+      await sendUserData(userData);
+
+      location.href = "../../index.html";
+    }
   } catch (err) {
     console.log("something went wrong", err);
   }
